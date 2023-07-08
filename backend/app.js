179ab84267a42d4e9e6195af37d580cfeda183f6
code-today <blogarpit21@gmail.com>
2023-07-08 23:53:50 +0530

    feat: Implement CRUD operations for Product

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 
+// Route Imports
+const product = require("./routes/productRoute");
+app.use("/api/v1", product);
+
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,82 @@
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" });
+const Product = require("../models/productModel");
+
+// Create Product
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// Get All Product
+exports.getAllProducts = async (req, res) => {
+  const product = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+// Get Product Details
+exports.getProductDetails = async (req, res, next) => {
+
+  const product = await Product.findById(req.params.id);
+
+    if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found!",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+// Update Product -- for Admin
+exports.updateProduct = async (req, res, next) => {
+  
+  let product = Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found!",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// Delete Product
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  await product.remove();
+
+  res.status(200).json({
+    success: ture,
+    message: "Product Delete Successfully",
+  });
 };
