89337052d43efd228754c07194797d867854a2d6
arpit-pathak <blogarpit21@gmail.com>
2023-07-16 18:54:18 +0530

    created user Controller, model and routes

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,15 @@ const express = require("express");
 
 const app = express();
 
+const errorMiddleware = require("./middleware/error");
 // Route Imports
 const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+
 app.use("/api/v1", product);
+app.use("/api/v1", user);
+
+// Middleware for Errors
+app.use(errorMiddleware)
 
 module.exports = app;
