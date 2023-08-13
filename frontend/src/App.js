f3b69ee5bb613e5751fad5c2121e9fae676c6b08
arpit-pathak <blogarpit21@gmail.com>
2023-08-13 23:52:35 +0530

    added Header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">ecommerce frontend</div>;
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
